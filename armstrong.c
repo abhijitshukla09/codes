f582301dd5aa4c7fbcf02af8723d56440aea18bf
abhijitshukla09 <abhijeetshukla240@gmail.com>
2025-08-04 09:04:14 +0530

    Create armstrong.c

---
+++ armstrong.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+    int count=0,num,sum=0,rem;
+    printf("enter any number");
+    num=n;
+    while(num!=0)
+    {
+        rem=num/10;
+        count++;
+    }
+    while(num!=0)
+    
+    return 0;
+}
